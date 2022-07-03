5ddebccf826b6169c0d382baf6119e1f1c585d29
Asaréel Dadiouari <54989878+AsareelDadiouari@users.noreply.github.com>
2022-07-03 00:38:36 +0000

    Updates

---
+++ precache-manifest.065e27745db503a14b95bfe5f72b5178.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247523b9e8325bbd9bb8bf9577ddac86",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "ce9d0dbd15ab22b21951",
+    "url": "/resume/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dcf9218a3a25b534700c",
+    "url": "/resume/static/css/main.8fa51f23.chunk.css"
+  },
+  {
+    "revision": "ce9d0dbd15ab22b21951",
+    "url": "/resume/static/js/2.41acd0bd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/resume/static/js/2.41acd0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf9218a3a25b534700c",
+    "url": "/resume/static/js/main.138e194b.chunk.js"
+  },
+  {
+    "revision": "110beccd39da32aa8d5e",
+    "url": "/resume/static/js/runtime-main.49716f41.js"
+  },
+  {
+    "revision": "d7d73f6160ee70721017221b4461b6df",
+    "url": "/resume/static/media/Birdstagram.d7d73f61.png"
+  },
+  {
+    "revision": "7fd474bdd402506012409c78a7dc8ef4",
+    "url": "/resume/static/media/SansationLight.7fd474bd.ttf"
+  },
+  {
+    "revision": "dd4cfdd042f33115d9e39b8b1c9f5122",
+    "url": "/resume/static/media/polyquiz.dd4cfdd0.png"
+  },
+  {
+    "revision": "92a9fcedcc5b7891ff028aed915464c4",
+    "url": "/resume/static/media/settings.92a9fced.png"
+  }
+]);
